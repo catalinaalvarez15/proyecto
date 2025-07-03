4125b7da9b045f3da0f192feb7d338d42bd84117
catalinaalvarez15 <catalinai.alvarez@pregrado.uoh.cl>
2025-07-03 00:50:51 -0400

    jajajaja

--- proyectoia.py
+++ proyectoia.py
@@ -1,1 +1,7 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import seaborn as sns
